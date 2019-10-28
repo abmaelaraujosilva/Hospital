6564468aab23531f2d7473f184927ce5f8e7c6b2
abmaelsilva02 <abmaelsilva02@gmail.com>
2019-10-28 18:32:56 -0300

    Add project files.

---
+++ Hospital.Domain/Entities/Paciente.cs
@@ -0,0 +1,70 @@
+﻿using Hospital.Shared.Entidades;
+using System;
+
+namespace Hospital.Domain.Entities
+{
+    public class Paciente : Entidade
+    {
+        public Paciente(string nome, string sobrenome, string cpf, string telefone, DateTime dataDeNacimento)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Telefone = telefone;
+            DataDeNacimento = dataDeNacimento;
+            Sobrenome = sobrenome;
+            if (string.IsNullOrEmpty(nome))
+                AddErros("Nome", "O campo Nome deve ter ao menos 1 caractere");
+            if (string.IsNullOrEmpty(sobrenome))
+                AddErros("Sobrenome", "O campo Sobrenome deve ter ao menos 1 caractere");
+            if (string.IsNullOrEmpty(telefone))
+                AddErros("Telefone", "O campo Telefone é obrigatorio");
+            if (string.IsNullOrEmpty(cpf))
+                AddErros("CPF", "O campo CPF é obrigatorio");
+            if (!IsCpf(cpf))
+                AddErros("CPF", "O CPF é invalido");
+        }
+
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+        public string CPF { get; private set; }
+        public string Telefone { get; private set; }
+        public DateTime DataDeNacimento { get; private set; }
+
+
+        public static bool IsCpf(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+    }
+}
---
+++ Hospital.Shared/Entidades/Entidade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hospital.Shared.Entidades
+{
+    public class Entidade
+    {
+        public Entidade(Guid id)
+        {
+            Id = id;
+        }
+        public Entidade()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+        public List<Tuple<string, string>> Erros { get; private set; }
+
+        public void AddErros(string chave, string erro)
+        {
+            Erros.Add(new Tuple<string, string>(chave, erro));
+        }
+    }
+}
